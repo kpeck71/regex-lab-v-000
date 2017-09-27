412919c2f942d7e12c1df638b31564661ddbce2c
Kristen Peck <kpeck71@gmail.com>
2017-09-27 01:06:55 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,8 +14,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  #if text.scan(/\b[A-Z]+???\b/).empty?
-#    false
+  if text.scan(/\b[A-Z]+???\b/).empty?
+    false
   #end
 end
 
