60e2e6b35c335bb8987f81db5cb57a20fc98eb48
Kristen Peck <kpeck71@gmail.com>
2017-09-27 00:58:53 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,5 +20,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/(\d+)(\d+)(\d+)/)
+  if phone.scan(/(\d+)(\d+)(\d+)/).empty?
+    false
 end
