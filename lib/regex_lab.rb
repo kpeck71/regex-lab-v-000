bb06887ff11b7fc0d5a13a96aaf22ad3f7504344
Kristen Peck <kpeck71@gmail.com>
2017-09-27 00:35:30 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,6 @@
 def starts_with_a_vowel?(word)
-  word.match(/\b[AEIOUaeiou][a-z]*\b/)
+  if word.scan(/\b[AEIOUaeiou][a-z]*\b/).empty?
+    false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
